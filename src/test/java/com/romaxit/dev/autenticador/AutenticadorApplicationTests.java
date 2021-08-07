a11ccedf5bd3cfeca5eb5412a7c2ff255df21552
cromero82 <soportecarlosromero@gmail.com>
2021-08-06 20:11:51 -0500

    commit inicial + gitignore update

---
+++ src/test/java/com/romaxit/dev/autenticador/AutenticadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaxit.dev.autenticador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
